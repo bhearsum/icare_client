58b32f6680bc09c48a126b6989a2271d1c14ab8a
Ben Hearsum <ben@hearsum.ca>
2021-01-28 21:59:34 -0500

    Make it possible to query a layout

--- src/icare_client/cli.py
+++ src/icare_client/cli.py
@@ -8,24 +8,50 @@ from .api import login
 @click.option("--server", required=True, type=str)
 @click.option("--username", required=True, type=str)
 @click.option("--password", required=True, type=str)
-@click.option("--child-id", required=True, type=int)
 @click.pass_context
-def cli(ctx, server, username, password, child_id):
+def cli(ctx, server, username, password):
     ctx.ensure_object(dict)
     ctx.obj["server"] = server
     ctx.obj["username"] = username
     ctx.obj["password"] = password
-    ctx.obj["child_id"] = child_id
 
 
 @cli.command()
 @click.pass_context
-def download(ctx):
+def layouts(ctx):
     server = ctx.obj["server"]
     username = ctx.obj["username"]
     password = ctx.obj["password"]
-    child_id = ctx.obj["child_id"]
 
     with requests.session() as session:
         token = login(session, server, username, password)
         session.headers["Authorization"] = f"Bearer {token}"
+        url = f"{server}/fmi/data/vLatest/databases/iCareMobileAccess/layouts"
+        r = session.get(url)
+        r.raise_for_status()
+        layout_names = [l["name"] for l in r.json()["response"]["layouts"][0]["folderLayoutNames"]]
+        print("\n".join(layout_names))
+
+
+@cli.command()
+@click.pass_context
+@click.option("--child-id", required=True, type=int)
+@click.argument("layout")
+def download(ctx, child_id, layout):
+    server = ctx.obj["server"]
+    username = ctx.obj["username"]
+    password = ctx.obj["password"]
+
+    with requests.session() as session:
+        token = login(session, server, username, password)
+        session.headers["Authorization"] = f"Bearer {token}"
+        url = f"{server}/fmi/data/vLatest/databases/iCareMobileAccess/layouts/{layout}/_find"
+        params = {
+            "query": [{
+                "child::childId": child_id,
+            }]
+        }
+        r = session.post(url, json=params)
+        r.raise_for_status()
+        import pprint
+        pprint.pprint(r.json())
